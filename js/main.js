e208148a72e7af107c2862206ef6c98c224cd0b6
Unknown <tannerthelin@gmail.com>
2018-01-09 11:14:26 -0700

    Changed headlines with javascript
    Also changed <li> elements to add actual text

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
 /*jslint browser:true*/
 "use strict";
 
+// Change title & subtitle from the Javascript
+document.getElementById("company-name").innerHTML = "avenue studios design";
+document.getElementById("company-slogan").innerHTML = "Nobody does it better than us.";
+
+
+// Add Date to page
 var d = new Date();
 
 document.getElementById("date").innerHTML = d;
